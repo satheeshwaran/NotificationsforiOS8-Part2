02ee7bc2171885f92089e53dcc19d18e730c6c26
Satheeshwaran J <satheeshwaran.j@cognizant.com>
2014-07-03 20:43:26 +0530

    Initial Commit
    Initial Commit

---
+++ NotificationsforiOS8/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  NotificationsforiOS8
+//
+//  Created by Satheeshwaran on 6/25/14.
+//  Copyright (c) 2014 Satheeshwaran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var localNotification:UILocalNotification = UILocalNotification()
+        
+        localNotification.alertAction = "Testing notifications on iOS8"
+        
+        localNotification.alertBody = "Woww it works!!"
+        
+        localNotification.fireDate = NSDate(timeIntervalSinceNow: 30)
+        
+        localNotification.category = "INVITE_CATEGORY";
+
+        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
